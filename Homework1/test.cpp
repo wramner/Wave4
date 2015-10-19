0202997b1768e8d27ba1c00ee724455c1f41a0ed
Jocke <joakim@tillas.net>
2015-10-19 16:38:56 +0200

    Jocke toktestar

---
+++ Homework1/test.cpp
@@ -0,0 +1,19 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Oct 19, 2015
+ *      Author: sebis
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Jocke testar" << endl;
+	return 0;
+}
+
+
+
+
