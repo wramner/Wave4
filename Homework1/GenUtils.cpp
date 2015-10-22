41da0f79f6ac1d99477687c71c20ac816cc51d67
Jocke <joakim@tillas.net>
2015-10-22 10:22:16 +0200

    Skeleton

---
+++ Homework1/GenUtils.cpp
@@ -0,0 +1,20 @@
+/*
+ * GenUtils.cpp
+ *
+ *  Created on: Oct 13, 2015
+ *      Author: sebis
+ */
+
+using namespace std;
+
+namespace seb {
+
+	namespace str {
+
+	}
+
+}
+
+
+
+
---
+++ Homework1/GenUtils.hpp
@@ -0,0 +1,21 @@
+/*
+ * GenUtils.hpp
+ *
+ *  Created on: Oct 13, 2015
+ *      Author: sebis
+ */
+
+#ifndef GENUTILS_HPP_
+#define GENUTILS_HPP_
+
+
+
+namespace seb {
+
+	namespace str {
+
+	}
+}
+
+
+#endif /* GENUTILS_HPP_ */
